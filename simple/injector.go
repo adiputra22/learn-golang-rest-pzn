1bafc9b2f4108dc70af35b03683700fb270fac36
adiputra22 <tuts.adiputra@gmail.com>
2026-09-03 11:49:08 +0700

    provider set

--- simple/injector.go
+++ simple/injector.go
@@ -23,3 +23,11 @@ func InitializeDatabaseRepository() *DatabaseRepository {
 
 	return nil
 }
+
+var fooSet = wire.NewSet(NewFooRepository, NewFooService)
+var barSet = wire.NewSet(NewBarRepository, NewBarService)
+
+func InitializeFooBarService() *FooBarService {
+	wire.Build(fooSet, barSet, NewFooBarService)
+	return nil
+}
