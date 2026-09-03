66ebc3b6ebe0309355774422219424141d644eee
adiputra22 <tuts.adiputra@gmail.com>
2026-09-03 11:40:04 +0700

    alias multiple parameter

--- simple/injector.go
+++ simple/injector.go
@@ -13,3 +13,13 @@ func InitializeService(isError bool) (*SimpleService, error) {
 
 	return nil, nil
 }
+
+func InitializeDatabaseRepository() *DatabaseRepository {
+	wire.Build(
+		NewDatabasePostgreSQL,
+		NewDatabaseMongoDB,
+		NewDatabaseRepository,
+	)
+
+	return nil
+}
